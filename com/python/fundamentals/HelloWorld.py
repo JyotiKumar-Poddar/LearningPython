ca5bf436c2a69b1dda0da7141c2c8cce2b6b28bb
johnny18445 <jyoti.johney@gmail.com>
2016-11-14 13:28:47 +0530

    Hello World Defining a fucntion in python

---
+++ com/python/fundamentals/HelloWorld.py
@@ -0,0 +1,15 @@
+'''
+Created on 14-Nov-2016
+
+@author: jyotipoddar
+'''
+
+if __name__ == '__main__':
+    pass
+    print("Hello, World")
+# Ways of defining a function which takes an argument.
+def printMessage(x):
+    print("Hello "+x)
+    
+printMessage("Jyoti")
+    
